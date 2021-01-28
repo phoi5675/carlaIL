2c839602b301db52cc53eae1ec4be2884d6b916c
강민재 <38933647+phoi5675@users.noreply.github.com>
2021-01-28 22:39:07 +0900

    Add files via upload

---
+++ Recorder.py
@@ -0,0 +1,82 @@
+import math
+from PIL import Image
+
+
+class Recorder(object):
+    # 순서는 carla IL 의 datasheet 순서를 따름
+    image = None
+
+    steer = 0.0
+    gas = 0.0
+    brake = 0.0
+    hand_brake = False
+    reverse_gear = False
+    steer_noise = 0.0  # noise 값은 학습에 영향 x
+    gas_noise = 0.0
+    brake_noise = 0.0
+    x_pos = 0.0
+    y_pos = 0.0
+    speed = 0.0
+    # 센서에서 얻는 데이터
+    collision_other = 0.0
+    collision_ped = 0.0
+    collision_car = 0.0
+    opposite_line_enter = 0.0
+    sidewalk_intersect = 0.0
+    acc_x = 0.0
+    acc_y = 0.0
+    acc_z = 0.0
+    platform_time = 0.0
+    game_time = 0.0  # not used
+    orientation_x = 0.0
+    orientation_y = 0.0
+    orientation_z = 0.0
+
+    high_level_com = 0
+    noise = False  # not used
+    camera = 0  # not used
+    angle = 0  # not used
+
+    @staticmethod
+    def record(world):
+        DATA_ARY = [Recorder.steer, Recorder.gas, Recorder.brake, Recorder.hand_brake, Recorder.reverse_gear,
+                    Recorder.steer_noise, Recorder.gas_noise, Recorder.brake_noise,
+                    Recorder.x_pos, Recorder.y_pos, Recorder.speed,
+                    Recorder.collision_other, Recorder.collision_ped, Recorder.collision_car,
+                    Recorder.opposite_line_enter, Recorder.sidewalk_intersect,
+                    Recorder.acc_x, Recorder.acc_y, Recorder.acc_z,
+                    Recorder.platform_time, Recorder.game_time,
+                    Recorder.orientation_x, Recorder.orientation_y, Recorder.orientation_z,
+                    Recorder.high_level_com, Recorder.noise, Recorder.camera, Recorder.angle]
+
+        v = world.player.get_velocity()
+        t = world.player.get_transform()
+        c = world.player.get_control()
+        acc = world.player.get_acceleration()
+
+        # 순서는 carla IL 의 datasheet 순서를 따름
+        Recorder.steer = c.steer
+        Recorder.gas = c.throttle
+        Recorder.brake = c.brake
+        Recorder.hand_brake = c.hand_brake
+        Recorder.reverse_gear = c.reverse
+
+        Recorder.x_pos = t.location.x
+        Recorder.y_pos = t.location.y
+        Recorder.speed = 3.6 * math.sqrt(v.x ** 2 + v.y ** 2 + v.z ** 2)
+
+        Recorder.acc_x = acc.x
+        Recorder.acc_y = acc.y
+        Recorder.acc_z = acc.z
+        Recorder.platform_time = world.hud.simulation_time
+        # Recorder.game_time
+        Recorder.orientation_x = t.rotation.roll
+        Recorder.orientation_y = t.rotation.pitch
+        Recorder.orientation_z = t.rotation.yaw
+        # TODO high-level command 는 나중에 입력 부분에서 설정해야 함
+
+        for _list in DATA_ARY:
+            print(_list, end=' ')
+        print('\n')
+        if Recorder.image is not None:  # 동기화 문제 때문에 image 가 None 상태 유지되는 경우 예방
+            Recorder.image.save('output/%.3f.png' % Recorder.platform_time)
---
+++ main.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+
+import carla
+from game import *
+from Recorder import *
+import argparse
+import pygame
+
+# ==============================================================================
+# -- find carla module ---------------------------------------------------------
+# ==============================================================================
+
+
+import glob
+import os
+import sys
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+
+def game_loop(args):
+    pygame.init()
+    pygame.font.init()
+    world = None
+    tick_double_time = False  # delta_seconds 의 두 배 시간으로 녹화하게 만듦
+    try:
+        # 서버 연결
+        client = carla.Client(args.host, args.port)
+        client.set_timeout(2.0)
+
+        # pygame 해상도 설정 / HWSURFACE, DOUBLEBUF 는 화면 플리커링 등 방지용
+        display = pygame.display.set_mode(
+            (args.width, args.height),
+            pygame.HWSURFACE | pygame.DOUBLEBUF)
+
+        hud = HUD(args.width, args.height)
+        # set synchronous mode
+        server_world = client.get_world()
+        settings = server_world.get_settings()
+        settings.synchronous_mode = True
+        settings.fixed_delta_seconds = 0.05
+
+        server_world.apply_settings(settings)
+
+        world = World(server_world, hud, args)
+
+        controller = KeyboardControl(world, args.autopilot)
+
+        clock = pygame.time.Clock()
+        while True:
+            # tick_busy_loop(FPS) : 수직동기화랑 비슷한 tick() 함수
+            clock.tick_busy_loop(60)
+            if controller.parse_events(client, world, clock):
+                return
+            world.tick(clock)
+            server_world.tick()  # 서버 시간 tick
+
+            # 화면 제외 다른 데이터 녹화
+            if world.recording_enabled and tick_double_time:
+                Recorder.record(world)
+                tick_double_time = False
+            if tick_double_time is False:  # 스위치 기능을 이용해 time_step 을 0.05초의 두 배로 설정
+                tick_double_time = True
+
+            world.render(display)
+            pygame.display.flip()
+
+    finally:
+        if world and world.recording_enabled:
+            client.stop_recorder()
+
+        if world is not None:
+            world.destroy()
+
+        pygame.quit()
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description='CARLA Manual Control Client')
+    argparser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        dest='debug',
+        help='print debug information')
+    argparser.add_argument(
+        '--host',
+        metavar='H',
+        default='127.0.0.1',
+        help='IP of the host server (default: 127.0.0.1)')
+    argparser.add_argument(
+        '-p', '--port',
+        metavar='P',
+        default=2000,
+        type=int,
+        help='TCP port to listen to (default: 2000)')
+    argparser.add_argument(
+        '-a', '--autopilot',
+        action='store_true',
+        help='enable autopilot')
+    argparser.add_argument(
+        '--res',
+        metavar='WIDTHxHEIGHT',
+        default='1280x720',
+        help='window resolution (default: 1280x720)')
+    argparser.add_argument(
+        '--filter',
+        metavar='PATTERN',
+        default='vehicle.*',
+        help='actor filter (default: "vehicle.*")')
+    argparser.add_argument(
+        '--rolename',
+        metavar='NAME',
+        default='hero',
+        help='actor role name (default: "hero")')
+    argparser.add_argument(
+        '--gamma',
+        default=2.2,
+        type=float,
+        help='Gamma correction of the camera (default: 2.2)')
+    args = argparser.parse_args()
+
+    args.width, args.height = [int(x) for x in args.res.split('x')]
+
+    log_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=log_level)
+
+    logging.info('listening to server %s:%s', args.host, args.port)
+
+    print(__doc__)
+
+    try:
+
+        game_loop(args)
+
+    except KeyboardInterrupt:
+        print('\nCancelled by user. Bye!')
+
+
+if __name__ == '__main__':
+    main()
+
